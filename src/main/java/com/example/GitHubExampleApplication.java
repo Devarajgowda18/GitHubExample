02a608231c001980e39b873ef6942724d4120735
Devaraj <demariya@in.ibm.com>
2016-10-05 12:17:19 +0530

    First Commit

---
+++ src/main/java/com/example/GitHubExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubExampleApplication.class, args);
+	}
+}
